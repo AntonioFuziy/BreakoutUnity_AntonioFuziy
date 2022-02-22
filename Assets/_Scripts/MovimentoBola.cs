855b3b96d635a52e230a50796db7e0b9e208b51a
AntonioFuziy <49486453+AntonioFuziy@users.noreply.github.com>
2022-02-21 21:57:13 -0300

    feat: added space start and hold ball until it starts

--- Assets/_Scripts/MovimentoBola.cs
+++ Assets/_Scripts/MovimentoBola.cs
@@ -11,6 +11,7 @@ public class MovimentoBola : MonoBehaviour
     public AudioSource collideSound;
     private Vector3 direcao;
     GameManager gm;
+    public bool activateGame = false;
 
     void Start()
     {
@@ -27,20 +28,31 @@ public class MovimentoBola : MonoBehaviour
     {
         if (gm.gameState != GameManager.GameState.GAME) return;
         
-        transform.position += direcao * Time.deltaTime * velocidade;
+        if(!activateGame){
+            Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+            transform.position = playerPosition + new Vector3(0, 0.5f, 0);
+        } else{
+            transform.position += direcao * Time.deltaTime * velocidade;
+            Vector2 posicaoViewport = Camera.main.WorldToViewportPoint(transform.position);
 
-        Vector2 posicaoViewport = Camera.main.WorldToViewportPoint(transform.position);
+            if( posicaoViewport.x < 0 || posicaoViewport.x > 1 ){
+                direcao = new Vector3(-direcao.x, direcao.y);
+            }
+            
+            if( posicaoViewport.y < 0 || posicaoViewport.y > 1 ){
+                direcao = new Vector3(direcao.x, -direcao.y);
+            }
+            
+            if(posicaoViewport.y < 0){
+                Reset();
+            }
+        }
 
-        if( posicaoViewport.x < 0 || posicaoViewport.x > 1 )
-        {
-            direcao = new Vector3(-direcao.x, direcao.y);
-        }
-        if( posicaoViewport.y < 0 || posicaoViewport.y > 1 )
-        {
-            direcao = new Vector3(direcao.x, -direcao.y);
-        }
-        if(posicaoViewport.y < 0){
-            Reset();
+        if(Input.GetKeyDown(KeyCode.Space)){
+            float dirX = Random.Range(-5.0f, 5.0f);
+            float dirY = Random.Range(2.0f, 5.0f);
+            direcao = new Vector3(dirX, dirY).normalized;
+            activateGame = true;
         }
     }
 
@@ -48,14 +60,8 @@ public class MovimentoBola : MonoBehaviour
         if(gm.vidas <= 0 && gm.gameState == GameManager.GameState.GAME){
             gm.ChangeState(GameManager.GameState.ENDGAME);
         }
-        
-        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
-        transform.position = playerPosition + new Vector3(0, 0.5f, 0);
 
-        float dirX = Random.Range(-5.0f, 5.0f);
-        float dirY = Random.Range(2.0f, 5.0f);
-
-        direcao = new Vector3(dirX, dirY).normalized;
+        activateGame = false;
         collideSound.Play();
         gm.vidas--;
     }
@@ -66,9 +72,7 @@ public class MovimentoBola : MonoBehaviour
             float dirY = Random.Range(1.0f, 5.0f);
 
             direcao = new Vector3(dirX, dirY).normalized;
-        }
-        else if(col.gameObject.CompareTag("Bloco"))
-        {
+        } else if (col.gameObject.CompareTag("Bloco")){
             direcao = new Vector3(direcao.x, -direcao.y);
             gm.pontos++;
         }
